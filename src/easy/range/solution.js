39f13a3048b2ad1dbf253aab83c426d10ac0edaf
Dave <Daverevell491@gmail.com>
2022-10-17 17:02:18 +0100

    Range completed

--- src/easy/range/solution.js
+++ src/easy/range/solution.js
@@ -4,11 +4,12 @@ function range(array) {
   // if (num1 > num2 && num1 > num3) {
   //   let comp1 = num1 - num2
   //   let comp2 = num1 - num3
-      
+  const difference = Math.max(...array) - Math.min(...array)
+  return difference
   // }
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] < Math.max(array)) {
-      console.log(array[i])
-    }
-  }
+  // for (let i = 0; i < array.length; i++) {
+    
+  //   return console.log(array[i])
+  // }
+  
 }
