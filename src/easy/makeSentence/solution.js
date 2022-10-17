2ae580607651480f898e8178c0540317011ad0da
Dave <Daverevell491@gmail.com>
2022-10-17 15:17:48 +0100

    secondsInHours completed

---
+++ src/easy/makeSentence/solution.js
@@ -0,0 +1,9 @@
+function secondsInHours(num1) {
+  num1 = num1 * 60^2
+  return num1
+}
+
+const hello = 10
+
+console.log(60^2)
+console.log("Hello")
---
+++ src/easy/secondsInHours/solution.js
@@ -0,0 +1,10 @@
+function secondsInHours(num1) {
+  num1 = num1 * (60**2)
+  num1 = num1 + " seconds"
+  return num1
+}
+
+const hello = 10
+
+console.log(60^2)
+console.log("Hello")
